c3f5dba82b17f1ef2a447d035a0e39eada5d93ad
Paul Frazee <pfrazee@gmail.com>
2019-03-18 16:53:14 -0500

    Remove dependencies and just use web-components directly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,4 @@
-import {LitElement, css, html} from '/vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
-
-class SiteProfile extends LitElement {
-  static get properties() {
-    return {
-      info: {type: Object}
-    }
-  }
-
+class SiteProfile extends HTMLElement {
   constructor () {
     super()
     this.info = null
@@ -16,35 +8,23 @@ class SiteProfile extends LitElement {
   async load () {
     var self = new DatArchive(window.location)
     this.info = await self.getInfo()
+    this.render()
   }
 
-  render() {
-    if (!this.info) return html``
-    return html`
+  render () {
+    this.innerHTML = `
       <img class="thumb" src="/thumb">
-      <h1 class="title">${this.info.title || 'Anonymous'}</h1>
-      <p class="description">${this.info.description}</p>
+      <h1 class="title"></h1>
+      <p class="description"></p>
       <p class="links">
         Links:
         <a href="intent:unwalled.garden/view-profile?url=${encodeURIComponent(window.location)}">Social Profile</a>,
         <a href="intent:unwalled.garden/edit-source?url=${encodeURIComponent(window.location)}">View Source</a>
       </p>
     `
+    this.querySelector('.title').textContent = this.info.title || 'Anonymous'
+    this.querySelector('.description').textContent = this.info.description || ''
   }
 }
-SiteProfile.styles = css`
-:host {
-  display: block;
-  text-align: center;
-  margin: 2em;
-}
-
-.thumb {
-  border-radius: 50%;
-  object-fit: cover;
-  width: 100px;
-  height: 100px;
-}
-`
 
 customElements.define('site-profile', SiteProfile)
